87437e73a5194d1bae07cef92c2f5135d121acc1
Cristian Pallarés <cristianp@wing.cloud>
2024-03-01 14:06:21 +0100

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,28 @@
 // @ts-check
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import { extensions } from "./src/extensions-rule.js";
 
 export default tseslint.config(
-	{
-		ignores: ["cdk.out/", "node_modules/"],
-	},
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
 	eslint.configs.recommended,
 	tseslint.configs.eslintRecommended,
 	...tseslint.configs.recommendedTypeChecked,
-	// ...tseslint.configs.strictTypeChecked,
 	...tseslint.configs.stylisticTypeChecked,
 	{
+		ignores: ["cdk.out/", "node_modules/"],
 		languageOptions: {
 			parserOptions: {
 				project: true,
 				tsconfigRootDir: import.meta.dirname,
 			},
 		},
+		plugins: {
+			extensions: { rules: await import("./eslint.extensions-rule.js") },
+		},
 		rules: {
 			"@typescript-eslint/consistent-type-imports": "error",
-			// "@typescript-eslint/no-unsafe-argument": "warn",
-			// "@typescript-eslint/no-unsafe-assignment": "warn",
 			"@typescript-eslint/require-await": "warn",
 			"@typescript-eslint/no-unused-vars": "warn",
-		},
-	},
-	{
-		plugins: {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-			extensions: { rules: { extensions } },
-		},
-		rules: {
 			"extensions/extensions": "error",
 		},
 	},
--- src/event-store/event-store.ts
+++ src/event-store/event-store.ts
@@ -2,17 +2,24 @@ import { Construct } from "constructs";
 import { DynamoEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 import { StartingPosition } from "aws-cdk-lib/aws-lambda";
 import {
+	HttpApi,
+	HttpIntegrationType,
+	type IHttpApi,
+} from "aws-cdk-lib/aws-apigatewayv2";
+import {
 	TableV2,
 	AttributeType,
 	StreamViewType,
 } from "aws-cdk-lib/aws-dynamodb";
 import { NodejsFunction, OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
-import { Topic } from "aws-cdk-lib/aws-sns";
+import { Topic, type ITopic } from "aws-cdk-lib/aws-sns";
 import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { StreamSequencer } from "./stream-sequencer.js";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 
 export class EventStore extends Construct {
-	readonly transactionsTopic: Topic;
+	readonly transactionsTopic: ITopic;
+	readonly api: IHttpApi;
 
 	constructor(scope: Construct, id: string) {
 		super(scope, id);
@@ -29,7 +36,6 @@ export class EventStore extends Construct {
 				// platform: "node",
 				// target: "node20",
 				mainFields: ["module", "main"],
-				externalModules: ["aws-lambda"],
 			},
 		});
 
@@ -54,6 +60,24 @@ export class EventStore extends Construct {
 			new SqsSubscription(streamSequencer.transactionsQueue),
 		);
 
+		const apiHandler = new NodejsFunction(this, "api-handler", {
+			entry: `${import.meta.dirname}/event-store.api-handler.ts`,
+			bundling: {
+				format: OutputFormat.ESM,
+				// platform: "node",
+				// target: "node20",
+				mainFields: ["module", "main"],
+			},
+		});
+
+		const api = new HttpApi(this, "api", {
+			defaultIntegration: new HttpLambdaIntegration(
+				"api-handler-integration",
+				apiHandler,
+			),
+		});
+
 		this.transactionsTopic = transactionsTopic;
+		this.api = api;
 	}
 }
